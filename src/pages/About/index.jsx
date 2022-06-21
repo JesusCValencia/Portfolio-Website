d08644995935701c884e88f5e906ad9720153546
JesusCValencia <jesuscalvalencia@gmail.com>
2022-06-21 13:04:12 -0400

    deploy update

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -10,11 +10,11 @@ const About = () => {
         <div className='infocontainer'>
             <br/>
             <div className = 'blogItem-wrap'>
-                <img className='blogItem-coverAbout' src={"Portfolio-Website/assets/images/lofi.gif"} alt='about-coverImage' />
+                <img className='blogItem-coverAbout' src="Portfolio-Website/assets/images/lofi.gif" alt='about-coverImage' />
             </div>
             <br/>
             <br/>
-            <p class = 'thick'>Caffeine free coder, pixel drawing enthusiast with a nature of a calm tech practilist. 
+            <p class = 'thick'>Caffeine free coder, pixel artist enthusiast with a nature of a calm tech practilist. 
                 Currently finishing my bachelor's degree in Computer Information Systems at USC Upstate, with an end goal as a SWE.
                 
                 
